8ce3e1bb829a0c0f0d0dfcf7055604594fdef40a
Cody Sherman <cody@codysherman.com>
2019-09-18 17:53:16 -0500

    make options save to localstorage

--- streamer/options.js
+++ streamer/options.js
@@ -9,83 +9,78 @@ var elements_ids = querySelectorAll("input, select").map(function(item) {
 
 var loading = document.getElementById("loading");
 
-// chrome.storage.sync.get(null, function(items) {
-//   elements_ids.forEach(function(id) {
-//     if (!items[id]) return;
+elements_ids.forEach(function(id) {
+  if (!window.localStorage.getItem(id)) return;
 
-//     var element = document.getElementById(id);
-//     if (items[id]) {
-//       if (element.type === "checkbox") {
-//         element.checked = items[id] === "true";
-//       } else {
-//         element.value = items[id];
-//       }
-//     } else {
-//       var item = {};
-//       item[element.id] =
-//         element.type === "checkbox"
-//           ? element.checked === true
-//             ? "true"
-//             : "false"
-//           : element.value;
+  var element = document.getElementById(id);
+  if (window.localStorage.getItem(id)) {
+    if (element.type === "checkbox") {
+      element.checked = window.localStorage.getItem(id) === "true";
+    } else {
+      element.value = window.localStorage.getItem(id);
+    }
+  } else {
+    var item = {};
+    item[element.id] =
+      element.type === "checkbox"
+        ? element.checked === true
+          ? "true"
+          : "false"
+        : element.value;
 
-//       try {
-//         chrome.storage.sync.set(item);
-//       } catch (e) {
-//         location.reload();
-//       }
-//     }
-//   });
-//   setTimeout(function() {
-//     loading.setAttribute("hidden", "");
-//   }, 600);
-// });
+    window.localStorage.setItem(
+      Object.keys(item)[0],
+      item[Object.keys(item)[0]]
+    );
+  }
+});
+setTimeout(function() {
+  loading.setAttribute("hidden", "");
+}, 600);
 
-// elements_ids.forEach(function(id) {
-//   var element = document.getElementById(id);
-//   element.onchange = function(e) {
-//     e && e.stopPropagation();
-//     element.disabled = true;
+elements_ids.forEach(function(id) {
+  var element = document.getElementById(id);
+  element.onchange = function(e) {
+    e && e.stopPropagation();
+    element.disabled = true;
 
-//     loading.querySelector("span").innerHTML = "Saving changes";
-//     loading.removeAttribute("hidden");
+    loading.querySelector("span").innerHTML = "Saving changes";
+    loading.removeAttribute("hidden");
 
-//     var item = {};
-//     item[element.id] =
-//       element.type === "checkbox"
-//         ? element.checked === true
-//           ? "true"
-//           : "false"
-//         : element.value;
+    var item = {};
+    item[element.id] =
+      element.type === "checkbox"
+        ? element.checked === true
+          ? "true"
+          : "false"
+        : element.value;
 
-//     try {
-//       chrome.storage.sync.set(item, function() {
-//         element.disabled = false;
-//         setTimeout(function() {
-//           loading.setAttribute("hidden", "");
-//         }, 600);
-//       });
-//     } catch (e) {
-//       location.reload();
-//     }
-//   };
+    window.localStorage.setItem(
+      Object.keys(item)[0],
+      item[Object.keys(item)[0]]
+    );
+    element.disabled = false;
+    setTimeout(function() {
+      loading.setAttribute("hidden", "");
+    }, 600);
+  };
 
-//   if (element.type) {
-//     element.parentNode.onclick = function() {
-//       if (element.type !== "checkbox") {
-//         element.focus();
-//       } else {
-//         element.checked = !element.checked;
-//       }
-//     };
+  if (element.type) {
+    element.parentNode.onclick = function() {
+      if (element.type !== "checkbox") {
+        element.focus();
+      } else {
+        element.checked = !element.checked;
+      }
+    };
 
-//     element.onclick = element.onfocus = function(e) {
-//       e && e.stopPropagation();
-//     };
-//   }
+    element.onclick = element.onfocus = function(e) {
+      e && e.stopPropagation();
+    };
+  }
 
-//   document.getElementById("done-button").onclick = function(e) {
-//     e.preventDefault();
-//     location.href = this.href;
-//   };
-// });
+  document.getElementById("done-button").onclick = function(e) {
+    e.preventDefault();
+    location.href = this.href;
+  };
+});
