72f863d04821e5c2af2fc9f819e5274062a0d0fe
Cody Sherman <meatwaddancin@gmail.com>
2020-01-07 16:36:34 -0600

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo '2n.fm' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:codysherman/2NFM.git master:gh-pages
+
+cd -
