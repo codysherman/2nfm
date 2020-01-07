c2bfc91723eaac79342a59f1d1bc8e9721bd697f
Cody Sherman <meatwaddancin@gmail.com>
2020-01-07 17:48:23 -0600

    add 404 redirect hack

--- src/main.js
+++ src/main.js
@@ -8,5 +8,12 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  }
 }).$mount("#app");
