48660ef8d4a9278515a109197e7538961e88ff4f
Cody Sherman <meatwaddancin@gmail.com>
2020-01-14 14:54:03 -0600

    refactor app.vue order

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<template>
-  <div id="app" class="frow-container height-100">
-    <router-view />
-  </div>
-</template>
-
 <style lang="sass">
 @import ~frow/_variables
 @import @/assets/sass/_colors.sass
 @import @/assets/sass/_variables.sass
 @import ~frow
 </style>
+
+<template>
+  <div id="app" class="frow-container height-100">
+    <router-view />
+  </div>
+</template>
