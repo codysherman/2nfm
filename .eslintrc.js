ea287886d9280764b3d438a50d914117d2df65aa
Sam Moore <sam@toldmedia.com>
2020-01-22 16:25:37 -0500

    improve ESLint rules to prevent JS errors & add required arrow-parens

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
     // we should always disable console logs and debugging in production
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ['error', 'always',],
+    'comma-dangle': ['error', 'always-multiline',],
+    'arrow-parens': ['error', 'always',],
     'indent': ['error', 2,],
     'max-len': ['error', { 'code': 100, 'tabWidth': 2, },],
     'quotes': ['error', 'single',],
