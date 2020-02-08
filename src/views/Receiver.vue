c6b020683dbeb9120b68a480dfff0390d365723a
Cody Sherman <meatwaddancin@gmail.com>
2020-02-08 02:42:37 -0600

    redirect sanitized urls

--- src/views/Receiver.vue
+++ src/views/Receiver.vue
@@ -217,6 +217,12 @@ export default {
       }
     },
   },
+  created() {
+    const sanitizedRoomId = this.$route.params.room.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9-_]/g, '').toLowerCase();
+    if (this.$route.params.room !== sanitizedRoomId) {
+      this.$router.push(sanitizedRoomId);
+    }
+  },
   mounted() {
     window.addEventListener(
       'offline',
