cdff966dc865a2392618e8c08bd34e5ff474fd9b
Cody Sherman <meatwaddancin@gmail.com>
2020-02-08 04:14:50 -0600

    bump dependencies

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: '@import "@/assets/sass/global-setup.sass"',
+        prependData: '@import "@/assets/sass/global-setup.sass"',
       },
     },
   },
