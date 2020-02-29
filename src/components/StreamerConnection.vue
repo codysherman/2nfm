71fb9ff4d17daf8ea13022fb86f9d78107e4f439
mphipps2017 <mnphipps98@gmail.com>
2020-02-29 13:41:52 -0600

    Updated logic for this.connection.leave and resolved #61

--- src/components/StreamerConnection.vue
+++ src/components/StreamerConnection.vue
@@ -255,7 +255,25 @@ export default {
       this.connection.open(this.connection.sessionid, roomOpenCallback);
 
       var oldLength = 0;
-      this.connection.onleave = this.connection.onPeerStateChanged = () => {
+      this.connection.onleave = () => {
+        let participants = this.connection.getAllParticipants();
+        var count = participants.length;
+        if (participants.includes(event.userid)) {
+          count--;
+        }
+        this.setViewerCount(count);
+      };
+
+      this.connection.onleave = (event) =>{
+        let participants = this.connection.getAllParticipants();
+        var count = participants.length;
+        if (participants.includes(event.userid)) {
+          count--;
+        }
+        this.setViewerCount(count);
+      };
+
+      this.connection.onPeerStateChanged = () => {
         var participantsCount = this.connection.getAllParticipants().length;
         if (oldLength != participantsCount) {
           // sendTabTitle();
