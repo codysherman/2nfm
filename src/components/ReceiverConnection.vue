fa7484083b49976d704493292b8b20de0a01d6dd
Tom Pocius <poci14@gmail.com>
2020-05-17 19:19:22 -0500

    started trying to map out how to pass if user is streaming audio or video

--- src/components/ReceiverConnection.vue
+++ src/components/ReceiverConnection.vue
@@ -150,6 +150,7 @@ export default {
 
     this.connection.onExtraDataUpdated = (e) => {
       this.$emit('receiverViewerCount', e.extra.receiverViewerCount);
+      this.$emit('systemAudioId', e.extra.systemAudioId);
     };
 
     // if user left
