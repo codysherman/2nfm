4c3bf7565186e0e30c71564e0138b4791d39d9ea
Cody Sherman <cody@codysherman.com>
2020-06-03 20:47:42 -0500

    update turn server urls

--- src/utils/background/helpers/IceServersHandler.js
+++ src/utils/background/helpers/IceServersHandler.js
@@ -3,19 +3,19 @@
 export const IceServersHandler = (function() {
   function getIceServers(withTurn = true) {
     var iceServers = [{
-      urls: [ 'stun:ws-turn1.xirsys.com' ],
+      urls: [ 'stun:us-turn7.xirsys.com' ],
     }];
 
     if (withTurn) iceServers.push({
       username: process.env.VUE_APP_TURN_USERNAME,
       credential: process.env.VUE_APP_TURN_CREDENTIAL,
       urls: [
-        'turn:us-turn1.xirsys.com:80?transport=udp',
-        'turn:us-turn1.xirsys.com:3478?transport=udp',
-        'turn:us-turn1.xirsys.com:80?transport=tcp',
-        'turn:us-turn1.xirsys.com:3478?transport=tcp',
-        'turns:us-turn1.xirsys.com:443?transport=tcp',
-        'turns:us-turn1.xirsys.com:5349?transport=tcp',
+        'turn:us-turn7.xirsys.com:80?transport=udp',
+        'turn:us-turn7.xirsys.com:3478?transport=udp',
+        'turn:us-turn7.xirsys.com:80?transport=tcp',
+        'turn:us-turn7.xirsys.com:3478?transport=tcp',
+        'turns:us-turn7.xirsys.com:443?transport=tcp',
+        'turns:us-turn7.xirsys.com:5349?transport=tcp',
       ],
     });
 
