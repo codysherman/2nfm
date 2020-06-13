5d5b6855127421147c5106e06c00f356b4943d87
Cody Sherman <cody@codysherman.com>
2020-06-13 12:34:28 -0500

    clean up stopsection css

--- src/components/Streamer/StopSection.vue
+++ src/components/Streamer/StopSection.vue
@@ -1,28 +1,27 @@
 <style scoped lang="sass">
-#stop-section
+.public-link
   text-align: center
+  display: inline-block
+  font-size: 40px
+  color: $primary-color
+  margin: 0
 
-  #public-link
-    display: inline-block
-    font-size: 40px
-    color: $primary-color
-    margin: 0 auto
+  // XS
+  @media (max-width: 767px)
+    font-size: 30px
 
-    // XS
-    @media (max-width: 767px)
-      font-size: 30px
+.copy-button
+  display: inline-block
+  margin-left: 14px
+  cursor: pointer
 
-  #copy-button
-    display: inline-block
-    margin-left: 14px
-    cursor: pointer
+  svg
+    width: 30px
+    fill: $primary-color
 
-    svg
-      width: 30px
-      fill: $primary-color
-
-  #copy-notification
-    font-size: 20px
+.copy-notification
+  font-size: 20px
+  text-align: center
 
 .public-indicator
   border-radius: $border-radius-small
@@ -40,28 +39,26 @@
   font-weight: $weight-bold
 
 .streamer-control-buttons
-  margin: 0 auto
-  font-size: 30px
-  padding: 15px
-  background: none
-  border: 3px solid $primary-color
-  color: $primary-color
-  border-radius: 10px
+  button
+    margin: 0 auto
+    font-size: 30px
+    padding: 15px
+    background: none
+    border: 3px solid $primary-color
+    color: $primary-color
+    border-radius: 10px
 </style>
 
 <template lang="pug">
-section#stop-section
-  //- router-link#public-link(
-  //-   to="{query: { s: sessionId, p: room_password }}`}" target="_blank"
-  //- )
-  router-link#public-link(:to="sessionId" target="_blank")
-    | {{ `2n.fm/${sessionId}` }}
-  #copy-button(@click="copyUrl()")
-      CopySvg
-  #copy-notification.animate-fade-in(v-if="copyNotification")
+div
+  .frow.row-center
+    router-link.public-link(:to="sessionId" target="_blank")
+      | {{ `2n.fm/${sessionId}` }}
+    .copy-button(@click="copyUrl()")
+        CopySvg
+  .copy-notification.animate-fade-in(v-if="copyNotification")
     | Copied
   .viewer-count.mt-20
-    span#viewer-count-number
     | {{ viewerCount }} {{ viewerCount === 1 ? 'Viewer' : 'Viewers' }}
   .frow.mt-5.mb-30
     .public-indicator(:class="{'public': privacy === 'public'}") {{privacy}}
@@ -71,15 +68,16 @@ section#stop-section
     :receiverViewerCount="viewerCount"
     :disableAutoplay="true"
   )
-  .frow.row-between
-    button.col-xs-2-5.streamer-control-buttons(type="button" @click="stopStream")
-      | End Sharing
-    button.col-xs-2-5.streamer-control-buttons(type="button" @click="showPreview = !showPreview")
-      | {{ showPreview ? 'Hide Preview' : 'Show Preview' }}
+  .streamer-control-buttons
+    .frow.row-between
+      button.col-xs-2-5(type="button" @click="stopStream")
+        | End Sharing
+      button.col-xs-2-5(type="button" @click="showPreview = !showPreview")
+        | {{ showPreview ? 'Hide Preview' : 'Show Preview' }}
   .frow.width-100.mt-20
     a.text-underline(
       href="https://caniuse.com/#search=getDisplayMedia"
-      rel="noreferrer"
+      rel="noopener"
       target="_blank"
     )
       | OS and Browser Limitations
@@ -120,27 +118,22 @@ export default {
       showPreview: false,
     };
   },
-  watch: {
-  },
   methods: {
     copyUrl() {
-      let copyText = document.getElementById('public-link');
-      let input = document.createElement('input');
-      input.setAttribute('value', `https://${copyText.innerText}`);
-      document.body.appendChild(input);
-      input.select();
-
+      let tempInput = document.createElement('input');
+      tempInput.setAttribute('value', `https://2n.fm/${this.sessionId}`);
+      document.body.appendChild(tempInput);
+      tempInput.select();
       let result = document.execCommand('copy');
-      const copyNotification = document.getElementById('copy-notification');
       if (result) {
-        document.body.removeChild(input);
         this.copyNotification = true;
         setTimeout(() => {
           this.copyNotification = false;
         }, 5000);
       } else {
-        copyNotification.innerText = 'Copy failed';
+        console.error('Copy failed');
       }
+      document.body.removeChild(tempInput);
     },
     stopStream() {
       this.$emit('stopStream');
