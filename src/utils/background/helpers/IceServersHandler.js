bfaea90473bdfa92c59fc8dd845e14c13e54c66e
Cody Sherman <cody@codysherman.com>
2020-06-27 13:14:10 -0500

    add missing TURN credential message

--- src/utils/background/helpers/IceServersHandler.js
+++ src/utils/background/helpers/IceServersHandler.js
@@ -6,18 +6,23 @@ export const IceServersHandler = (function() {
       urls: [ 'stun:us-turn7.xirsys.com' ],
     }];
 
-    if (withTurn) iceServers.push({
-      username: process.env.VUE_APP_TURN_USERNAME,
-      credential: process.env.VUE_APP_TURN_CREDENTIAL,
-      urls: [
-        'turn:us-turn7.xirsys.com:80?transport=udp',
-        'turn:us-turn7.xirsys.com:3478?transport=udp',
-        'turn:us-turn7.xirsys.com:80?transport=tcp',
-        'turn:us-turn7.xirsys.com:3478?transport=tcp',
-        'turns:us-turn7.xirsys.com:443?transport=tcp',
-        'turns:us-turn7.xirsys.com:5349?transport=tcp',
-      ],
-    });
+    if (withTurn) {
+      if (!process.env.VUE_APP_TURN_USERNAME || !process.env.VUE_APP_TURN_CREDENTIAL) {
+        console.warn('Missing TURN login credentials');
+      }
+      iceServers.push({
+        username: process.env.VUE_APP_TURN_USERNAME,
+        credential: process.env.VUE_APP_TURN_CREDENTIAL,
+        urls: [
+          'turn:us-turn7.xirsys.com:80?transport=udp',
+          'turn:us-turn7.xirsys.com:3478?transport=udp',
+          'turn:us-turn7.xirsys.com:80?transport=tcp',
+          'turn:us-turn7.xirsys.com:3478?transport=tcp',
+          'turns:us-turn7.xirsys.com:443?transport=tcp',
+          'turns:us-turn7.xirsys.com:5349?transport=tcp',
+        ],
+      });
+    }
 
     return iceServers;
   }
