81aadc330d5fa8d092e78ed146b6db88de00e794
Tom <tom@pop-os.localdomain>
2020-06-30 15:41:03 -0500

    passed stream to extracontrols and console log to track where room description was, remove consolelog before pr

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -39,6 +39,7 @@
     v-if="showExtraControls"
     :autoplay.sync="autoplay"
     :receiverViewerCount="receiverViewerCount"
+    :stream="stream"
   )
 </template>
 
@@ -111,6 +112,7 @@ export default {
       }
     },
     onStream() {
+      console.log('121212', this.stream.roomDescription);
       if (typeof this.stream.mute === 'function' && !navigator.mozGetUserMedia) {
         // HACK: Avoids double audio in Chrome-based browsers
         console.warn('Avoiding double audio in Chrome via RTCMultiConnection#mute');
