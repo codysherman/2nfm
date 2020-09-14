fc543d7eb579f96dda6992930b347b12a18c5988
Cody Sherman <cody@codysherman.com>
2020-09-14 11:18:56 -0500

    alphabatize eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
-  root: true,
   env: {
     // this section will be used to determine which APIs are available to us
     // (i.e are we running in a browser environment or a node.js env)
-    node: true,
     browser: true,
+    node: true,
   },
+  extends: [
+    // use the recommended rule set for both plain javascript and vue
+    'eslint:recommended',
+    'plugin:vue/recommended',
+  ],
   globals: {
     RTCMultiConnection: true,
   },
@@ -14,14 +18,10 @@ module.exports = {
     // specifying a module sourcetype prevent eslint from marking import statements as errors
     sourceType: 'module',
   },
-  extends: [
-    // use the recommended rule set for both plain javascript and vue
-    'eslint:recommended',
-    'plugin:vue/recommended',
-  ],
   plugins: [
     'import',
   ],
+  root: true,
   rules: {
     // we should always disable console logs and debugging in production
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -30,10 +30,10 @@ module.exports = {
     // must expand out comma-dangle to get `functions` option enabled
     'comma-dangle': ['error', {
       'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'always-multiline',
       'exports': 'always-multiline',
+      'imports': 'always-multiline',
       'functions': 'always-multiline',
+      'objects': 'always-multiline',
     }],
     'import/extensions': ['error', 'ignorePackages', {'js': 'never'}],
     'indent': ['error', 2, { 'SwitchCase': 1 }],
