2cf19480d24ecde892355a8413b3e82000cd105e
pociust <poci14.com>
2021-11-09 15:11:40 -0600

    logic to toggle theme from dark to light

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,59 @@
 @import ~frow
 @import @/assets/sass/global.sass
 
+.theme-svg
+  height: 25px
+  width: auto
+  position: fixed
+  left: 10px
+  top: 10px
+  &:hover
+    cursor: pointer
 .min-100vh
   min-height: 100vh
 </style>
 
 <template lang="pug">
   #app.frow-container
+    LightThemeSun.theme-svg(v-if="theme === 'light'" @click="toggleDarkTheme")
+    DarkThemeMoon.theme-svg(v-if="theme === 'dark'" @click="toggleDarkTheme")
+    div {{ theme }}
     router-view.frow.centered.min-100vh.py-20(:key="$route.fullPath")
 </template>
 
 <script>
+import LightThemeSun from '@/assets/svgs/light-theme-sun.svg';
+import DarkThemeMoon from '@/assets/svgs/dark-theme-moon.svg';
 export default {
   name: 'App',
   metaInfo: { title: '2N.fm' },
+  components: {
+    LightThemeSun,
+    DarkThemeMoon,
+  },
+  data: function () {
+    return { theme: 'light' };
+  },
+  created() {
+    document.documentElement.setAttribute(
+      'data-theme', localStorage.getItem('theme') || 'dark',
+    );
+    this.theme = document.documentElement.getAttribute('data-theme');
+  },
+  methods: {
+    toggleDarkTheme() {
+      if (
+        !document.documentElement.getAttribute('data-theme')
+        || document.documentElement.getAttribute('data-theme') === 'light'
+      ) {
+        this.theme = 'dark';
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        this.theme = 'light';
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
+      localStorage.setItem('theme', document.documentElement.getAttribute('data-theme'));
+    },
+  },
 };
 </script>
