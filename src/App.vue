ce14c7e4d7bd78173f51141006bb058dfdd81372
pociust <poci14.com>
2021-11-12 16:33:41 -0600

    added placeholder svg for auto theme, changed method into watch with switch condition to setAttribute for different themes

--- src/App.vue
+++ src/App.vue
@@ -23,45 +23,52 @@
 <template lang="pug">
   #app.frow-container
     transition(name="bounce-in")
-      LightThemeSun.theme-svg(v-if="theme === 'light'" @click="toggleDarkTheme")
+      AutoTheme.theme-svg(v-if="theme === 'auto'" @click="theme='dark'")
     transition(name="bounce-in")
-      DarkThemeMoon.theme-svg(v-if="theme === 'dark'" @click="toggleDarkTheme")
+      DarkThemeMoon.theme-svg(v-if="theme === 'dark'" @click="theme='light'")
+    transition(name="bounce-in")
+      LightThemeSun.theme-svg(v-if="theme === 'light'" @click="theme='auto'")
     router-view.frow.centered.min-100vh.py-20(:key="$route.fullPath")
 </template>
 
 <script>
 import LightThemeSun from '@/assets/svgs/light-theme-sun.svg';
 import DarkThemeMoon from '@/assets/svgs/dark-theme-moon.svg';
+import AutoTheme from '@/assets/svgs/auto_theme.svg';
 export default {
   name: 'App',
   metaInfo: { title: '2N.fm' },
   components: {
     LightThemeSun,
     DarkThemeMoon,
+    AutoTheme,
   },
   data: function () {
     return { theme: 'light' };
+  },
+  watch: {
+    theme: function (theme) {
+      switch (theme) {
+        case 'light':
+          document.documentElement.setAttribute('data-theme', 'light');
+          break;
+        case 'dark':
+          document.documentElement.setAttribute('data-theme', 'dark');
+          break;
+        case 'auto':
+          document.documentElement.setAttribute('data-theme', 'auto');
+          break;
+        default:
+          document.documentElement.setAttribute('data-theme', 'auto');
+      }
+      localStorage.setItem('theme', document.documentElement.getAttribute('data-theme'));
+    },
   },
   created() {
     document.documentElement.setAttribute(
       'data-theme', localStorage.getItem('theme') || 'light',
     );
     this.theme = document.documentElement.getAttribute('data-theme');
-  },
-  methods: {
-    toggleDarkTheme() {
-      if (
-        !document.documentElement.getAttribute('data-theme')
-        || document.documentElement.getAttribute('data-theme') === 'light'
-      ) {
-        this.theme = 'dark';
-        document.documentElement.setAttribute('data-theme', 'dark');
-      } else {
-        this.theme = 'light';
-        document.documentElement.setAttribute('data-theme', 'light');
-      }
-      localStorage.setItem('theme', document.documentElement.getAttribute('data-theme'));
-    },
   },
 };
 </script>
